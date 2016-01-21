ea5f4edf50d28437073ec34e0d2cc4a8fcd71b98
Quashon <quashon@student.fdu.edu>
2016-01-20 20:43:18 -0500

    added question mark in first line and hyperlink to mission

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
   </div>
 </div>
 <div class="container">
-  <p>So you're interested in the Green Team. You've come to the right place--here you can find out more about us and register for our mailing list so that you can actively make a difference on our campus.</p>
-  <p>If you're unsure as to what exactly the Green Team is about, review our Mission Statement or reach out to us--attend a meeting and see if it's for you! Or just take advantage of our events, checkout our regularly updated event calendar, connect it to your Google Calendar and receive updates right to your mobile device!</p>
+  <p>So you're interested in the Green Team? You've come to the right place--here you can find out more about us and register for our mailing list so that you can actively make a difference on our campus.</p>
+  <p>If you're unsure as to what exactly the Green Team is about, review our <a href="http://fdugreenteam.org/mission.php">Mission Statement</a> or reach out to us--attend a meeting and see if it's for you! Or just take advantage of our events, checkout our regularly updated event calendar, connect it to your Google Calendar and receive updates right to your mobile device!</p>
   <p>Together, let's enjoy ourselves while making our campus a more environmentally conscious place.</p>
 </div>
 <? require 'php/bottom.php' ?>
